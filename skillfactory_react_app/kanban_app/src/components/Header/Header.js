cd2d333a146bf3eb522d96d58968788a08493d24
dilyin9 <dilyin9@gmail.com>
2022-10-24 00:15:12 +0300

    24/0

---
+++ skillfactory_react_app/kanban_app/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+
+
+export default function Header() {
+  return (
+    <div className="header">
+      <label className="app_text header_text">Awesome Kanban Board</label>
+      <UserWindow />
+    </div>
+  )
+}
