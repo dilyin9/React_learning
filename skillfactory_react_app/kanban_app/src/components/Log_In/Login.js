932ee4b440963b5e9debe705f4d1c7cd2c1ac6ef
dilyin9 <dilyin9@gmail.com>
2022-10-27 01:24:40 +0300

    bbb

--- skillfactory_react_app/kanban_app/src/components/Log_In/Login.js
+++ skillfactory_react_app/kanban_app/src/components/Log_In/Login.js
@@ -1,14 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Popup from "./Popup";
+import {useNavigate} from "react-router-dom";
 
 export default function Login() {
   const [isShown, setIsShown] = useState(true);
-
+  
+  const navigate = useNavigate()
+  const handleSubmit =(e) => {
+    const newUser = {
+      user_name: e,
+      isCurrent: true,
+      data: []
+    }
+    localStorage.setItem(e, JSON.stringify(newUser));
+   // navigate('main');
+  }
   return (
     <div className="login">
       <Header />
-      <Popup isShown={isShown} />
+      <Popup isShown={isShown} formSubmitted={handleSubmit}/>
     </div>
   )
 }
--- skillfactory_react_app/kanban_app/src/components/Log_In/Popup.js
+++ skillfactory_react_app/kanban_app/src/components/Log_In/Popup.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Popup({isShown}) {
+export default function Popup({isShown,formSubmitted}) {
+  const [userName, setUserName] = useState('')
+  function handleChange(e) {
+    e.preventDefault();
+    formSubmitted(userName);
+  }
   return(
     <div className="login_popup">
       <div className="form_box">
-        <form>
+        <form onSubmit={handleChange}>
           <h2 className="app_text second_header_text">Sign In</h2>
           <label className="app_text popup_text">Login</label>
           <br></br>
-          <input className="popup_text popup_input" placeholder="password"></input>
+          <input onChange={(e) => {setUserName(e.target.value)}} className="popup_text popup_input" placeholder="username" value={userName}></input>
           <br></br>
-          <input type="submit" value="Login"></input>
+          <input className="popup_text popup_submit" type="submit" value="Login"></input>
         </form>
       </div>
     </div>
