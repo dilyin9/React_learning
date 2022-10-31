598acb0ff1aebddbfedb9af6e10ec5fcc01f6ff0
Dmitry Ilyin <dmitry.ilyin@digitaltwin.ru>
2022-10-31 18:22:53 +0300

    tt

--- skillfactory_react_app/kanban_app/src/components/Main/Main.js
+++ skillfactory_react_app/kanban_app/src/components/Main/Main.js
@@ -1,17 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Body from '../Body/Body';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
 export default function Main() {
+  const active_user = JSON.parse(localStorage.getItem('active_user'));
   const [userData, setUserData] = useState(
     JSON.parse(localStorage.getItem(JSON.parse(localStorage.getItem('active_user'))))
   )
+  useEffect(()=> {
+    localStorage.setItem(active_user, JSON.stringify(userData))
+  }, [userData])
   const handleBacklog = (obj) => {
     console.log(userData)
     let newData = [...userData.data]
     newData[0].tasks = obj;
     setUserData({...userData, data: newData})
+
   }
   return (
     <div className="App">
